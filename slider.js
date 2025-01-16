96a6c9ff03b48ebf8540c17bea47a7cf4464f0bd
leoliao57 <0796pl@gmail.com>
2025-01-16 13:27:52 +0800

    update done

--- slider.js
+++ slider.js
@@ -1,3 +1,11 @@
+// 當網頁載入完畢準備完成後執行箭頭函式
 $(document).ready(() => {
-    console.log("網頁載入完畢");
+    // 點擊事件:點下任何一個 .slider都會執行
+    $(".slider").click(function() {
+        // 刪除全部的 .slider類別 active
+        $(".slider").removeClass("active");
+        // 添加點下去的 .slider類別 active
+        $(this).addClass("active");
+
+    });
 });
